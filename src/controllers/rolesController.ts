07b563bec0eab4caff07263c5507bdc93ccf64d5
Miguel <angeloquenta@gmail.com>
2024-09-05 19:37:29 -0400

    feat adding Create, Read and Update for Roles

---
+++ src/controllers/rolesController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { handleError } from '../handlers/errorHandler';
+import { sendSuccess } from '../handlers/successHandler';
+import * as RolesInteractor from '../interactors/rolesInteractor';
+import Rol from '../models/rol';
+
+export const getRoles = async (req: Request, res: Response) => {
+  const rolName = req.body.rolName;
+  try {
+    const roles = await RolesInteractor.getRoles(rolName);
+    if (!roles) {
+      return res.status(404).json({ success: false, message: 'role not found' });
+    }
+    sendSuccess(res, roles, 'Roles retrieved successfully');
+  } catch (error) {
+    if (error instanceof Error) {
+      handleError(res, error);
+    }
+  }
+};
+
+export const createRol = async (req: Request, res: Response) => {
+  const newRol: Rol = req.body;
+  try {
+    const rol = await RolesInteractor.createRol(newRol);
+    if (!rol) {
+      return res.status(404).json({ success: false, message: 'can not create rol' });
+    }
+    sendSuccess(res, rol, 'Roles retrieved successfully');
+  } catch (error) {
+    if (error instanceof Error) {
+      handleError(res, error);
+    }
+  }
+};
+
+export const editRol = async (req: Request, res: Response) => {
+  const rolToEdit: Rol = req.body;
+  const id: number = parseInt(req.params.id);
+  try {
+    const editedRol = await RolesInteractor.editRol(rolToEdit, id);
+    if (!editedRol) {
+      return res.status(404).json({ success: false, message: 'can not edit rol' });
+    }
+    sendSuccess(res, editedRol, 'Roles edited successfully');
+  } catch (error) {
+    if (error instanceof Error) {
+      handleError(res, error);
+    }
+  }
+};
