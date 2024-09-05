42bc29a8fad8e010421072a10d31d2a82534d841
andrezuu <sanchezandres8203@gmail.com>
2024-09-05 18:50:21 -0400

    chore: add seed on events, interns, events_interns

--- data/migrations/20240829141549_newTables.ts
+++ data/migrations/20240829141549_newTables.ts
@@ -34,6 +34,7 @@ export async function up(knex: Knex): Promise<void> {
       table.integer('duration_hours');
       table.integer('max_interns');
       table.integer('min_interns');
+      table.integer('assigned_hours');
       table.timestamp('start_date');
       table.timestamp('end_date');
       table.timestamp('registration_deadline');
@@ -43,8 +44,18 @@ export async function up(knex: Knex): Promise<void> {
       table.timestamp('updated_at').defaultTo(knex.fn.now(6));
     })
     .createTable(tableNameEventsInterns, function (table) {
-      table.integer('intern_id').unsigned().references('id').inTable(tableNameIntern).onDelete('CASCADE');
-      table.integer('event_id').unsigned().references('id').inTable(tableNameEvents).onDelete('CASCADE');
+      table
+        .integer('intern_id')
+        .unsigned()
+        .references('id')
+        .inTable(tableNameIntern)
+        .onDelete('CASCADE');
+      table
+        .integer('event_id')
+        .unsigned()
+        .references('id')
+        .inTable(tableNameEvents)
+        .onDelete('CASCADE');
       table.enu('type', ['accepted', 'rejected', 'pending', 'reserve']).notNullable();
       table.timestamp('created_at').defaultTo(knex.fn.now(6));
       table.timestamp('updated_at').defaultTo(knex.fn.now(6));
