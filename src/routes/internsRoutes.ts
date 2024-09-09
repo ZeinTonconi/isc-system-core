ecf6da46f32f0fd535637dc1b62a728550372758
LushoCallapa <luiscallapa2004@gmail.com>
2024-09-08 20:33:21 -0400

    Task009: InternInformations

--- src/routes/internsRoutes.ts
+++ src/routes/internsRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { updateHoursController, getInternsById,getRecordInternsController } from "../controllers/internController";
+import { updateHoursController, getInternsById,getRecordInternsController, getInformationsInternController } from "../controllers/internController";
 
 const router = Router();
 
 router.route('/:intern_id/update-hours').put(updateHoursController);
 router.route('/:intern_id').get(getInternsById);
 router.route('/:intern_id/historial-eventos').get(getRecordInternsController);
+router.route('/:intern_id/informacion').get(getInformationsInternController)
 
 export default router;
