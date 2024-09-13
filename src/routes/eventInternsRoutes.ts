458b40f55c1f5c57957f4a86e3f678df4628c787
LushoCallapa <luiscallapa2004@gmail.com>
2024-09-13 14:36:58 -0400

    checkUserAuth in routes

--- src/routes/eventInternsRoutes.ts
+++ src/routes/eventInternsRoutes.ts
@@ -12,12 +12,12 @@ import {
 
 const router = Router();
 
-router.route('/:id_evento/register').post(registerInternController);
-router.route('/:id_evento/registrations').get(getEventInternsController);
-router.route('/:id_evento/update-status/:id_becario').put(updateInternType);
-router.route('/:id_evento/registrations/:id_becario').delete(deleteRegistrationController);
-router.route('/:id_evento/actualizar-eventos').put(updateEventHistoryController);
-router.route('/:id_evento/confirm_attendance/:id_becario').put(updateAttendanceController);
-router.route('/register-information').get(getEventInformationsController);
+router.route('/:id_evento/register').post(checkUserAuth, registerInternController);
+router.route('/:id_evento/registrations').get(checkUserAuth, getEventInternsController);
+router.route('/:id_evento/update-status/:id_becario').put(checkUserAuth, updateInternType);
+router.route('/:id_evento/registrations/:id_becario').delete(checkUserAuth, deleteRegistrationController);
+router.route('/:id_evento/actualizar-eventos').put(checkUserAuth, updateEventHistoryController);
+router.route('/:id_evento/confirm_attendance/:id_becario').put(checkUserAuth, updateAttendanceController);
+router.route('/register-information').get(checkUserAuth, getEventInformationsController);
 
 export default router;
--- src/routes/internsRoutes.ts
+++ src/routes/internsRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getInternsController, updateHoursController, getInternsById,getRecordInternsController, getInformationsInternController, getMyEventsInternController } from "../controllers/internController";
+import { checkUserAuth } from "../middlewares/checkUserAuth";
 
 const router = Router();
 
-router.route('/:intern_id/update-hours').put(updateHoursController);
-router.route('/:intern_id').get(getInternsById);
-router.route('/:intern_id/historial-eventos').get(getRecordInternsController);
-router.route('/:intern_id/my-events').get(getMyEventsInternController);
-router.route('/:intern_id/informacion').get(getInformationsInternController);
-router.route('/').get(getInternsController);
+router.route('/:intern_id/update-hours').put(checkUserAuth, updateHoursController);
+router.route('/:intern_id').get(checkUserAuth, getInternsById);
+router.route('/:intern_id/historial-eventos').get(checkUserAuth, getRecordInternsController);
+router.route('/:intern_id/my-events').get(checkUserAuth, getMyEventsInternController);
+router.route('/:intern_id/informacion').get(checkUserAuth, getInformationsInternController);
+router.route('/').get(checkUserAuth, getInternsController);
 
 export default router;
