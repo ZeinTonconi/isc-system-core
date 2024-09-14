995c1c46e91d72bff27122524f3f668aec402a72
andrezuu <sanchezandres8203@gmail.com>
2024-09-14 12:54:18 -0400

    temp: stopped using permissions on login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import * as LoginInteractor from '../interactors/loginInteractor';
-import * as PermissionsInteractor from '../interactors/permissionsInteractor'
+import * as PermissionsInteractor from '../interactors/permissionsInteractor';
 import { sendSuccess } from '../handlers/successHandler';
 import { handleError } from '../handlers/errorHandler';
 import { AuthenticationError } from '../errors/authenticationError';
@@ -9,14 +9,15 @@ import UserResponse from '../models/userResponseInterface';
 const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    const resLogin: string|UserResponse = await LoginInteractor.login(email, password);
+    const resLogin: string | UserResponse = await LoginInteractor.login(email, password);
     if (typeof resLogin === 'string') {
       throw new AuthenticationError(resLogin);
     }
-    const roles_permissions = await PermissionsInteractor.getRolesAndPermissions(resLogin.id)
+    // TODO: use when all permissions are set
+    // const roles_permissions = await PermissionsInteractor.getRolesAndPermissions(resLogin.id)
     const result = {
       ...resLogin,
-      roles_permissions,
+      // roles_permissions,
     };
     sendSuccess(res, result, 'Successfully logged in');
   } catch (error) {
